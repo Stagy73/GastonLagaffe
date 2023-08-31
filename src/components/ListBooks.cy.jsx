99656f2398f6881fafac3e2abd4d5828d85c688e
Nils <nils.mehlhorn.fr@gmail.com>
2023-08-31 11:18:16 +0200

    test

--- src/components/ListBooks.cy.jsx
+++ src/components/ListBooks.cy.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import { mount } from "@cypress/react";
-import ListBooks from "./ListBooks";
+import ListBooks, { bookData } from "./ListBooks";
 
 describe("ListBooks Component", () => {
-  it("renders table rows with download buttons", () => {
+  it("renders book rows and triggers file download", () => {
+    cy.intercept("**/*.cbr").as("download");
+
     cy.mount(<ListBooks />);
 
-    cy.get("tr")
-      .eq(0)
-      .within(() => {
-        cy.contains("Gaston 00 Gaffes et Gadgets");
-        cy.contains("Download File").click();
+    // Check each book row
+    cy.get("tbody tr").each((row, index) => {
+      const book = bookData[index];
+      cy.wrap(row).within(() => {
+        cy.contains(book.title);
+        cy.get("button").contains("Download File").click(); // Click the download button
       });
+
+      // Wait for the download to complete
+      cy.wait("@download", { timeout: 30000 }).then((interception) => {
+        expect(interception.response.statusCode).to.equal(200);
+
+        // Assert that the correct file is downloaded
+        const expectedFileName = book.fileLink.slice(
+          book.fileLink.lastIndexOf("/") + 1
+        );
+        cy.readFile(`./cypress/downloads/${expectedFileName}`).should("exist");
+      });
+    });
   });
 });
--- src/components/ListBooks.jsx
+++ src/components/ListBooks.jsx
@@ -1,90 +1,47 @@
+function BookRow({ title, fileLink }) {
+  const handleDownload = () => {
+    window.location.href = fileLink;
+  };
+
+  return (
+    <tr>
+      <td>{title}</td>
+      <td>
+        <button onClick={handleDownload}>Download File</button>
+      </td>
+    </tr>
+  );
+}
+const bookData = [
+  { title: "Gaffes et Gadgets", fileLink: "./00.cbr" },
+  { title: "Gala de gaffe a gogo", fileLink: "./01.cbr" },
+  { title: "Le bureau des gaffes en gros", fileLink: "./02.cbr" },
+  { title: "Gare au gaffes du gars gonflé", fileLink: "./03.cbr" },
+  { title: "en direct de lagaffe", fileLink: "./04.cbr" },
+  { title: "Le lourd passé de lagaffe", fileLink: "./05.cbr" },
+  { title: "des gaffes et des degats", fileLink: "./06.cbr" },
+  { title: "un gaffeur sachant gaffer", fileLink: "./07.cbr" },
+  { title: "lagaffe nous gate", fileLink: "./08.cbr" },
+  { title: "le cas lagaffe", fileLink: "./09.cbr" },
+  { title: "le geant de la gaffe", fileLink: "./10.cbr" },
+  { title: "gaffes, bevues et boulettes", fileLink: "./11.cbr" },
+  { title: "le gang des gaffeurs", fileLink: "./12.cbr" },
+  { title: "lagaffe merite des baffes", fileLink: "./13.cbr" },
+  { title: "la saga des gaffes", fileLink: "./14.cbr" },
+  { title: "Gaffes a lagaffe", fileLink: "./15.cbr" },
+  { title: "N19", fileLink: "./16.cbr" },
+  { title: "HS gaston et le marsupilami", fileLink: "./17.cbr" },
+  { title: "le livre d'or", fileLink: "./18.cbr" },
+];
+
 function ListBooks() {
   return (
     <div>
       <table>
         <tbody>
-          <tr>
-            <td>Gaston 00 Gaffes et Gadgets</td>
-            <td>
-              <button
-                className="download-button"
-                onClick={() => {
-                  // Handle download logic here
-                  window.location.href = "./01.cbr";
-                }}
-              >
-                Download File
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
+          {bookData.map((book, index) => (
+            <BookRow key={index} title={book.title} fileLink={book.fileLink} />
+          ))}
         </tbody>
       </table>
     </div>
@@ -92,3 +49,4 @@ function ListBooks() {
 }
 
 export default ListBooks;
+export { bookData };
