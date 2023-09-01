dcb7cf76efe9361acb5f916f744c422520deda17
Cathy <ptipoison@yahoo.fr>
2023-09-01 14:55:28 +0200

    site validate

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,23 @@ import ListBooks from "./components/ListBooks";
 import Navbar from "./components/Navbar";
 import APropos from "./components/APropos";
 import Contact from "./components/Contact";
-import "./components/Navbar.css";
-
-import SlideshowWithAds from "./components/SideBarLeft";
-import ContainerEmpty from "./components/Container";
+//import SlideshowWithAds from "./components/SideBarLeft";
+//import ContainerEmpty from "./components/Container";
 import Footer from "./components/Footer";
-import FeedPosts from "./components/aff";
+//import FeedPosts from "./components/aff";
 import "./App.css";
+import "./components/Navbar.css";
 
 function App() {
   return (
-    <div className="divfooter">
-      <div>
-        <div className="mainapp">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<ListBooks />} />
-            <Route path="/APropos" element={<APropos />} />
-            <Route path="/Contact" element={<Contact />} />
-          </Routes>
-
-          <ContainerEmpty />
-          <div className="flexfooteraff">
-            <SlideshowWithAds />
-          </div>
-        </div>
-      </div>
-      <FeedPosts />
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ListBooks />} />
+        <Route path="/APropos" element={<APropos />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+      <ListBooks />
       <Footer />
     </div>
   );
