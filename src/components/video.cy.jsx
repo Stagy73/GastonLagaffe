2971e73391dfc2bfe0b9e3c2ff0da730e9410350
Nils <nils.mehlhorn.fr@gmail.com>
2023-09-01 09:43:26 +0200

    test api fix

--- src/components/video.cy.jsx
+++ src/components/video.cy.jsx
@@ -1,10 +1,11 @@
 // cypress/integration/api.spec.js
-const apiKey = "AIzaSyBjRypU1PzBlrkv2V4o2Ijo6R6EHKOKMeo";
+const apiKey = import.meta.env.VITE_API_KEY;
+
 describe("API Tests", () => {
   it("should fetch videos from the YouTube API", () => {
     cy.request(
       "GET",
-      "https://www.googleapis.com/youtube/v3/search?key=AIzaSyBjRypU1PzBlrkv2V4o2Ijo6R6EHKOKMeo&channelId=UCrcptvCXVgLlieu9pNe0Nqg&q=Gaston%20Lagaffe&order=date&part=snippet&type=video&maxResults=10"
+      `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=UCrcptvCXVgLlieu9pNe0Nqg&q=Gaston%20Lagaffe&order=date&part=snippet&type=video&maxResults=10`
     ).then((response) => {
       expect(response.status).to.eq(200); // Check if the request was successful
       expect(response.body.items).to.be.an("array"); // Check if the response contains an array of items
